67e5a3810003a30e1f970c3900d0162129da4e3e
grumblefoot <rowanosmon@gmail.com>
2023-09-07 15:21:01 -0700

    Began adding additional dice.

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,9 @@ class Dice extends StatelessWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int _currentDiceNo = 1;
+  int _numberOfDice = 1; // the number of dice to be rolled
+
+  List<int?> _diceResults = List.filled(25,null);
 
   void _incrementCounter() {
     setState(() {
@@ -81,8 +84,11 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-      _currentDiceNo = Random().nextInt(6) + 1; // Generate a new random value
-    });
+      for (int i = 0; i < _numberOfDice; i++) {
+        _currentDiceNo = Random().nextInt(6) + 1; // Generate a new random value
+        _diceResults[i] = _currentDiceNo;
+      }
+     });
   }
   @override
   Widget build(BuildContext context) {
@@ -98,41 +104,76 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ), Dice(dice_no: _currentDiceNo),// Pass the current dice_no value
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              TextField(
+                style: Theme.of(context).textTheme.headlineMedium,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: 'Number of Dice (n)',
+                ),
+                onChanged: (value) {
+
+                  setState(() {
+                    _numberOfDice = int.parse(value);
+                  });
+                },
+              ),
+              GridView.builder(
+                shrinkWrap: true,
+                itemCount: 25,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 5,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  if (index < _counter) {
+                    return Dice(dice_no: _diceResults[index] ?? 1);
+                  } else {
+                    return Container(); // Empty container for unused cells
+                  }
+                },
+              ),
+            ],
+          ),
+
         ),
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
